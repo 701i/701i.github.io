7049ce4615a0acaca3a343708044c5e2ea56827e
701i <anxelilk@gmail.com>
2022-08-16 06:15:17 -0500

    page begins in the bottom

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+//scroll hacia arriba
+    function scrollToBottom() {
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+    history.scrollRestoration = "manual";
+    window.onload = scrollToBottom;
+
 //agregar boton pantalla completa
+var text = document.querySelector('.text')
+function scrolListener(e){
+  var screenTop = document.scrollingElement.scrollTop;
+  var screenBottom = screenTop + innerHeight;
+  var textTop = text.getBoundingClientRect().top
+  
+  if ( textTop < screenBottom && textTop < screenTop)
+  {
+    text.children[0].classList.add("showtext");
+    text.children[0].classList.remove("hidetext");
+  }
+  
+}
+
+
+document.onscroll = scrolListener
